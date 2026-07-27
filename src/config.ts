f495e5c7735226b5f7adbdce3982b767d8283ffa
francis <work@francisyzy.com>
2026-07-27 11:18:59 +0800

    feat: add OWNER_TELEGRAM_ID and OWNER_USERNAME to config

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,9 @@ dotenv.config();
 const config = {
   API_TOKEN: process.env.API_TOKEN,
   WATERMARK: process.env.CHANNEL || "watermark",
-  CHANNEL: process.env.CHANNEL
+  CHANNEL: process.env.CHANNEL,
+  OWNER_TELEGRAM_ID: process.env.OWNER_TELEGRAM_ID,
+  OWNER_USERNAME: process.env.OWNER_USERNAME
 };
 
 export default config;
